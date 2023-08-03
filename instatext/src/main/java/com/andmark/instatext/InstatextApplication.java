4f2754dff8bde02d225fac8b72dbdeb5bae52ba0
Andrei Markov <noteandmark@gmail.com>
2023-08-03 13:31:30 +0300

    initial instatext commit

---
+++ instatext/src/main/java/com/andmark/instatext/InstatextApplication.java
@@ -0,0 +1,13 @@
+package com.andmark.instatext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstatextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstatextApplication.class, args);
+	}
+
+}
---
+++ instatext/src/test/java/com/andmark/instatext/InstatextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andmark.instatext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstatextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
